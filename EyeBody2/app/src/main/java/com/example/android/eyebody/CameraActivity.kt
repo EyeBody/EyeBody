6be638bcb343d7717e6fc8377e5005714e00caf3
mijey <yeaji21@gmail.com>
2017-09-09 12:03:13 +0900

    camera branch 생성

--- EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
@@ -3,6 +3,11 @@ package com.example.android.eyebody
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+/*
+ * 카메라(사진촬영, 카메라가이드)
+ * 카메라, 외장메모리 저장 권한
+ * camera preview에서 SurfaceView 클래스 사용하면 이미지를 오버레이 할 수 있음
+ */
 class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
