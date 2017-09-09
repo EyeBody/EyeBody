f8360be24ff64d4ffe7c17227af7dac48c10ad5c
ytw1122 <ytw1122@outlook.kr>
2017-09-10 00:04:32 +0900

    camera

--- EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
@@ -8,5 +8,6 @@ class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
     }
 }
