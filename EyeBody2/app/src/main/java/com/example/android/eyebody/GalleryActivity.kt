c235ddf92842f51ce9904416c59501e3bef67394
mijey <yeaji21@gmail.com>
2017-09-09 12:05:57 +0900

    gallery branch 생성

--- EyeBody2/app/src/main/java/com/example/android/eyebody/GalleryActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/GalleryActivity.kt
@@ -3,6 +3,12 @@ package com.example.android.eyebody
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+/*
+이미지 암호화
+file IO stream으로 이미지 AES 암호화/복호화
+
+갤러리 UI도 만들어야 함
+ */
 class GalleryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
