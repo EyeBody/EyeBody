a045fb6cd95072b63372ac0348d9376096dfbd88
ytw1122 <ytw1122@outlook.kr>
2017-09-13 22:22:36 +0900

    camera
    카메라 프리뷰 화면 만드는 과정
    
    안드로이드 스튜디오 api 23이상부터는 퍼미션을 주는 방식이 기존과 달라지면서 permission을 주는 코드작성중

--- EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
@@ -1,52 +1,92 @@
 package com.example.android.eyebody
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.graphics.PixelFormat
 import android.hardware.Camera
+import android.os.Build
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.annotation.RequiresApi
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.SurfaceHolder
 import kotlinx.android.synthetic.main.activity_camera.*
 import java.io.IOException
-
 /*
  * 카메라(사진촬영, 카메라가이드)
  * 카메라, 외장메모리 저장 권한
  * camera preview에서 SurfaceView 클래스 사용하면 이미지를 오버레이 할 수 있음
  */
-class CameraActivity : AppCompatActivity() , SurfaceHolder.Callback {
-    val stringPath = "/sdcard/sampleVideo."
-    private var surfaceHolder:SurfaceHolder=cameraScreen.holder
-    private var camera: android.hardware.Camera?=Camera.open()
-    var previewing:Boolean=false
+
+class CameraActivity : Activity(), SurfaceHolder.Callback {
+    companion object {
+        val REQUEST_PERMISSION = 1
+    }
+    private var surfaceHolder: SurfaceHolder? = null
+    private var camera: Camera? = null
+    private var previewing: Boolean = false
+    @RequiresApi(Build.VERSION_CODES.M)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
-
-        surfaceHolder?.addCallback(this)
-        surfaceHolder?.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
-
-        if(camera!=null){
-            try {
-                camera?.setPreviewDisplay(surfaceHolder)
-                camera?.startPreview()
-                previewing=true;
-            }catch (e: IOException){
-                e.printStackTrace()
+        window.setFormat(PixelFormat.UNKNOWN)
+        surfaceHolder = cameraScreen.holder
+        surfaceHolder!!.addCallback(this)
+        surfaceHolder!!.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+    }
+    override fun onStart() {
+        super.onStart()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_PERMISSION)
+        } else {
+            camera()
+        }
+    }
+    private fun camera(){
+        if (!previewing) {
+            camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK)
+            if (camera != null) {
+                try {
+                    camera!!.setPreviewDisplay(surfaceHolder)
+                    camera!!.startPreview()
+                    previewing = true
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSION -> if (grantResults.size > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                camera()
             }
         }
     }
     override fun surfaceCreated(p0: SurfaceHolder?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         camera=Camera.open()
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
     override fun surfaceDestroyed(p0: SurfaceHolder?) {
+
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        camera?.stopPreview()
-        camera?.release()
-        camera=null
-        previewing=false
     }
     override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+        if(previewing){
+            camera.stopPreview()
+            previewing=false
+        }
+        if(camera!=null){
+            try{
+                camera.setPreviewDisplay(surfaceHolder)
+                camera.startPreview()
+                previewing=true
+            }catch (e:IOException){
+                e.printStackTrace()
+            }
+        }
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
 }
