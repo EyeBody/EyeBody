f55bd603eaf7e34ed95ef9bd19935a61aa1a107d
mijey <yeaji21@gmail.com>
2017-09-14 11:40:50 +0900

    외부저장소 전용파일 폴더 생성

--- EyeBody2/app/src/main/java/com/example/android/eyebody/GalleryActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/GalleryActivity.kt
@@ -8,12 +8,9 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_gallery.*
 import android.graphics.BitmapFactory
 import android.os.Environment
+import android.util.Log
 import android.widget.Toast
 import java.io.*
-import java.security.Key
-import javax.crypto.Cipher
-import javax.crypto.spec.SecretKeySpec
-
 
 /*
 이미지 암호화
@@ -27,12 +24,30 @@ class GalleryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
 
+        var state: String = Environment.getExternalStorageState()
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            textView.text = "외부 저장소 사용가능"
+            var filedir: String = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString() + "/gallery_body"
+            textView.text = filedir
+
+            var file: File = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "gallery_body")
+            Toast.makeText(this, "디렉토리가 만들어짐", Toast.LENGTH_SHORT).show()
+            if (!file.mkdirs()) {
+                //Log.e(LOG_TAG, "Directory not created")
+                Toast.makeText(this, "디렉토리가 만들어지지 않음", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun getImageFromAssets(){
         //assets에서 이미지 불러오기
         var assetManager: AssetManager = getAssets()  //AssetManager 생성
         var inputStream: InputStream = assetManager.open("gallery_body/front_week1.jpg")    //InputStream으로 변환
         var bitmapImg: Bitmap = BitmapFactory.decodeStream(inputStream)  //Bitmap으로 변환
         imageView.setImageBitmap(bitmapImg)   //Bitmap까지 변환해야 이미지뷰에 띄울 수 있음
+    }
 
+    fun InternalFileIO(){
         //내부 저장소에 파일 읽고쓰기
         var fileName: String = "eyebody.txt"
 
@@ -44,7 +59,7 @@ class GalleryActivity : AppCompatActivity() {
                 var outputStream: FileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
                 outputStream.write(string.toByteArray())
                 outputStream.close()
-                Toast.makeText(this, "쓰기 완료", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getFilesDir().toString() + "쓰기 완료", Toast.LENGTH_SHORT).show()
             } catch(e: Exception){
                 e.printStackTrace()
                 Toast.makeText(this, "쓰기 실패", Toast.LENGTH_SHORT).show()
@@ -75,7 +90,5 @@ class GalleryActivity : AppCompatActivity() {
                 //Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
             }
         }
-
-
     }
 }
