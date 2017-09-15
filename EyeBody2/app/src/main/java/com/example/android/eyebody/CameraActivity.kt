6c8cf7545f90c86ddccad39ce802158efe01f8ac
ytw1122 <ytw1122@outlook.kr>
2017-09-15 16:10:07 +0900

    camera source
    카메라 소스
    카메라 프리뷰

--- EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
@@ -1,15 +1,9 @@
 package com.example.android.eyebody
 
-import android.Manifest
 import android.app.Activity
-import android.content.pm.PackageManager
 import android.graphics.PixelFormat
 import android.hardware.Camera
-import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.view.SurfaceHolder
 import kotlinx.android.synthetic.main.activity_camera.*
 import java.io.IOException
@@ -20,73 +14,41 @@ import java.io.IOException
  */
 
 class CameraActivity : Activity(), SurfaceHolder.Callback {
-    companion object {
-        val REQUEST_PERMISSION = 1
-    }
     private var surfaceHolder: SurfaceHolder? = null
     private var camera: Camera? = null
     private var previewing: Boolean = false
-    @RequiresApi(Build.VERSION_CODES.M)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         window.setFormat(PixelFormat.UNKNOWN)
         surfaceHolder = cameraScreen.holder
-        surfaceHolder!!.addCallback(this)
-        surfaceHolder!!.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
-    }
-    override fun onStart() {
-        super.onStart()
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_PERMISSION)
-        } else {
-            camera()
-        }
-    }
-    private fun camera(){
-        if (!previewing) {
-            camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK)
-            if (camera != null) {
-                try {
-                    camera!!.setPreviewDisplay(surfaceHolder)
-                    camera!!.startPreview()
-                    previewing = true
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-        }
-    }
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            REQUEST_PERMISSION -> if (grantResults.size > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                camera()
-            }
-        }
-    }
-    override fun surfaceCreated(p0: SurfaceHolder?) {
-        camera=Camera.open()
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-    override fun surfaceDestroyed(p0: SurfaceHolder?) {
-
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        surfaceHolder?.addCallback(this)
+        surfaceHolder?.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
     }
     override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
-        if(previewing){
-            camera.stopPreview()
-            previewing=false
+        if (previewing) {
+            camera?.stopPreview()
+            previewing = false
         }
-        if(camera!=null){
-            try{
-                camera.setPreviewDisplay(surfaceHolder)
-                camera.startPreview()
-                previewing=true
-            }catch (e:IOException){
+        if (camera != null) {
+            try {
+                camera?.setPreviewDisplay(surfaceHolder)
+                camera?.startPreview()
+                previewing = true
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
+    }
+    override fun surfaceCreated(holder:SurfaceHolder) {
+        camera = Camera.open()
+    }
+    override fun surfaceDestroyed(holder:SurfaceHolder) {
+        camera?.stopPreview()
+        camera?.release()
+        camera = null
+        previewing = false
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
