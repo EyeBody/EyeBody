5947f741091b83719de445ecb101664c520c1b95
Yoon Myenggeun <morebob@naver.com>
2017-09-17 22:42:35 +0900

    before attach fragment
    1. main activity 레이아웃에 fragment를 이용하여 패스워드 화면을 띄울 예정
    2. 또는 현재 배경 위에 (어두운 배경+패스워드입력창) 을 frameLayout을 여러번 겹쳐서 표현하는 것을 생각중이나 일단 frgament는 어디서든 잘 쓰일 것 같아 공부해야 할 것 같음

--- EyeBody2/app/src/main/java/com/example/android/eyebody/InitActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/InitActivity.kt
@@ -5,16 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Debug
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_init.*
-import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
 
 class InitActivity : AppCompatActivity() {
 
@@ -23,44 +20,68 @@ class InitActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init)
 
-        val goMain by lazy{ Intent(this,MainActivity::class.java) }
+        val goMain by lazy { Intent(this, MainActivity::class.java) }
+        val tv_password = findViewById<EditText>(textview_password.id)
+        val bt_passwordSubmit = findViewById<Button>(button_passwordSubmit.id)
+
+
+        // TODO ----- 현재는 비밀번호를 hash로 변환하여 저장하고 있음.
+        // TODO ----> 비밀번호 검증 : hash / 이미지 암호화 : AES n-bit (key = hash) / 백업 : hash
+        // 유저가 비밀번호를 치면 hash값으로 변환 후 이 값을 이용하여 암호화/복호화
+        // 어떻게 해야댈지 모르겠네 아.. 이건 너무 구린데.. 넘나 어렵당
+
 
         /* SharedPreferences (앱 공유 데이터)
         isUserTypeInitSetting : 유저가 처음 시작할 때 비밀번호, 몸매목표 등을 세팅했는지 확인하는 파일
         MODE_PRIVATE : 다른 앱이 접근 불가(파일 권한 없이 불가를 뜻하는 것 같음) (mode_world_readable : 다른 앱이 공유 데이터에 접근 가능)
          */
-        // TODO("공유데이터로 initActivity를 실행하게? 아니면 이닛에서 공유데이터를 판별할지 해야함")
-        // TODO("현재는 비밀번호를 그대로 저장하지만 암호화기법을 사용하여 앱을 뜯었을 때도 알 수 없게 해야 함."
-        val share : SharedPreferences = getSharedPreferences("pref1", Context.MODE_PRIVATE)
-        val isSetPassword = share.getBoolean("isSetting",false)
-        if(isSetPassword) {
-            Log.d("initt","비밀번호 초기설정이 완료되어있으므로 MainActivity로 넘어갑니다.")
+        val share: SharedPreferences = getSharedPreferences("hash-md5", Context.MODE_PRIVATE)
+        val isSetPassword = share.getBoolean("isSetting", false)
+
+        if (isSetPassword) {
+            Log.d("mydbg_init", "비밀번호 초기설정이 완료되어있으므로 MainActivity로 넘어갑니다.")
             startActivity(goMain)
             finish()
         }
 
-        val tv_password = findViewById<EditText>(textview_password.id)
-        val bt_passwordSubmit = findViewById<Button>(button_passwordSubmit.id)
 
-        val str_password = tv_password.text
+        tv_password.setOnEditorActionListener { textView, i, keyEvent ->
+            true
+            bt_passwordSubmit.callOnClick()
+        }
 
-        bt_passwordSubmit.setOnClickListener {
 
-            Log.d("initt","password >>>> ${tv_password.text.toString()}")
+        bt_passwordSubmit.setOnClickListener { view ->
+            val str_pw = tv_password.text
+            Log.d("mydbg_init", "password >>>> $str_pw")
 
-            if(tv_password.text != null){
+            if (str_pw.isNotEmpty()) {
+                Toast.makeText(this, "${str_pw}를 입력하였습니다.", Toast.LENGTH_LONG).show()
+                Log.d("mydbg_init", "${str_pw.length}")
+
+
+                // TODO ----- MD5는 보안이 취약해 추후 바꿔야 함. SHA-3(KECCAK)을 후보로 생각하고 있음.
+                // 파라미터로 charset을 넣지 않으면 값이 보존되지 않는 현상에 대해 알아봐야 함.
+                val md5 = MessageDigest.getInstance("MD5")
+                val pwByte = str_pw.toString().toByteArray(charset("unicode"))
+                md5.update(pwByte)
+                val hashedPW = md5.digest().toString(charset("unicode"))
+
+                Log.d("mydbg_init", "평문 pw : ${pwByte.toString(charset("unicode"))}")
+                Log.d("mydbg_init", "MD5 pw : $hashedPW")
+
+
                 val Editor = share.edit()
-                Editor.putString("password",str_password.toString())
-                        .putBoolean("isSetting",true)
+                Editor.putString("hashedPW", hashedPW)
+                        .putBoolean("isSetting", true)
                         .commit()
                 startActivity(goMain)
                 finish()
-                // TODO("왜 안죽어")
+
             } else {
-                Toast.makeText(this,"password를 입력해주세요 (1자 이상)", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "password를 입력해주세요 (1자 이상)", Toast.LENGTH_LONG).show()
             }
         }
-
 
     }
 }
