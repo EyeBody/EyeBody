bcc8b13da5a80214b9a389cc51b32c029f6e0d98
ytw1122 <ytw1122@outlook.kr>
2017-09-24 20:33:12 +0900

    카메라 버튼 구현
    카메라 버튼 구현

--- EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/CameraActivity.kt
@@ -1,18 +1,22 @@
 package com.example.android.eyebody
 
 import android.app.Activity
-import android.content.ContentValues.TAG
+import android.content.ContentValues
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.PixelFormat
 import android.hardware.Camera
 import android.hardware.Camera.PictureCallback
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.Surface
 import android.view.SurfaceHolder
 import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.FileNotFoundException
 import java.io.IOException
+import java.io.OutputStream
 
 /*
  * 카메라(사진촬영, 카메라가이드)
@@ -20,18 +24,21 @@ import java.io.IOException
  * camera preview에서 SurfaceView 클래스 사용하면 이미지를 오버레이 할 수 있음
  */
 class CameraActivity : Activity(), SurfaceHolder.Callback {
-   // var TAG:String="CameraActivity"
+    var TAG: String = "CameraActivity"
     private var surfaceHolder: SurfaceHolder? = null
     private var camera: Camera? = null
     private var previewing: Boolean = false
-    var controlInflator: LayoutInflater? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         init()
         btn_shutter.setOnClickListener {
-            camera?.takePicture(shutterCallback, rawCallback, jpegCallback)
+            try {
+                camera?.takePicture(shutterCallback, rawCallback, jpegCallback)
+            }catch(e:RuntimeException){
+                Log.d(TAG,"take picture failed")
+            }
         }
     }
     private var shutterCallback: Camera.ShutterCallback = Camera.ShutterCallback {
@@ -39,41 +46,32 @@ class CameraActivity : Activity(), SurfaceHolder.Callback {
             Log.d(TAG, "onShutter'd")
         }
     }
-    var rawCallback=Camera.PictureCallback { bytes: ByteArray, camera: Camera ->
-        fun onPictureTaken(data: ByteArray, camera: Camera) {
+    private var rawCallback = Camera.PictureCallback { bytes: ByteArray?, camera: Camera? ->
+        fun onPictureTaken(bytes: ByteArray, camera: Camera) {
             Log.d(TAG, "onPictureTaken-raw")
         }
     }
-    var jpegCallback=PictureCallback{ bytes: ByteArray, camera: Camera ->
-        fun onPictureTaken(data:ByteArray,camera:Camera){
-            var bitmapPicture=BitmapFactory.decodeByteArray(data,0,bytes.size)
-            Log.d(TAG,"onPictureTaken - jp")
+    private var jpegCallback = PictureCallback { bytes: ByteArray?, camera: Camera? ->
+        fun onPictureTaken(bytes: ByteArray, camera: Camera) {
+            var bitmapPicture: Bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            Log.d(TAG, "onPictureTaken - jp")
+            var uriTarget: Uri = contentResolver.insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, ContentValues())
+            var imageFile: OutputStream? = null
+            try {
+                imageFile = contentResolver.openOutputStream(uriTarget)
+                imageFile.write(bytes)
+                imageFile.flush()
+                imageFile.close()
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
+        camera?.startPreview()
     }
-    /*
-    private var jpegCallback= Camera.PictureCallback() { bytes: ByteArray, camera: Camera ->
-        fun onPictureTaken(data: ByteArray, camera: Camera) {
-            var width: Int = camera.parameters.pictureSize.width
-            var height: Int = camera.parameters.pictureSize.height
 
-            var orientation: Int = setCameraDisplayOrientation(this, CAMERA_FACING_BACK, camera)
-            var options: BitmapFactory.Options = BitmapFactory.Options()
-            options.inPreferredConfig = Bitmap.Config.ARGB_8888
-            var bitmap: Bitmap = BitmapFactory.decodeByteArray(data, 0, data.size, options)
-
-            var matrix = Matrix().postRotate(orientation.toFloat())
-            bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true)
-
-            var stream = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-            var currentData = stream.toByteArray()
-
-            SaveImageTask().execute(currentData)
-            resetCam()
-            Log.d(TAG, "onPictureTaken-jpeg")
-        }
-    }*/
-        //셔터 버튼이 눌리면 실행.
+    //셔터 버튼이 눌리면 실행.
     //setCameraDisplayOrientation 함수 -> 카메라가 회전하는대로 카메라도 회전하도록 설정
     fun setCameraDisplayOrientation(activity: Activity, cameraId: Int, camera: Camera): Int {
         var info = android.hardware.Camera.CameraInfo()
@@ -86,7 +84,7 @@ class CameraActivity : Activity(), SurfaceHolder.Callback {
             Surface.ROTATION_180 -> degree = 180
             Surface.ROTATION_270 -> degree = 270
         }
-        var result: Int
+        var result: Int=0
         if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
             result = (info.orientation + degree) % 360
             result = (360 - result) % 360
@@ -95,18 +93,13 @@ class CameraActivity : Activity(), SurfaceHolder.Callback {
         }
         return result
     }
+
     private fun init() {
         window.setFormat(PixelFormat.UNKNOWN)
         surfaceHolder = cameraScreen.holder
         surfaceHolder?.addCallback(this)
         surfaceHolder?.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
-        controlInflator = LayoutInflater.from(baseContext)
-
-// IMAGE_FILE=sdf?.format(date)
-    }
-
-    private fun setFileName() {
-
+        //controlInflator = LayoutInflater.from(baseContext)
     }
 
     override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
