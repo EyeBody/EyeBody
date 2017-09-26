88ed9b2aa67595d4dc57d23ef20000a5e214abd1
mijey <yeaji21@gmail.com>
2017-09-26 20:16:54 +0900

    (gallery)RecyclerView로 사진 넘기기

--- EyeBody2/app/src/main/java/com/example/android/eyebody/GalleryActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/GalleryActivity.kt
@@ -1,21 +1,24 @@
 package com.example.android.eyebody
 
-import android.app.ActionBar
-import android.content.Context
+import android.app.Activity
 import android.content.res.AssetManager
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_gallery.*
-import android.graphics.BitmapFactory
 import android.os.Environment
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
-import android.view.ViewGroup
+import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import java.io.*
 
+//TODO gallery UI 예쁘게 다듬기
+//TODO image metadata로 1주전 2주전 정보 표시
+
 class GalleryActivity : AppCompatActivity() {
+    var photoList = ArrayList<Photo>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
@@ -32,9 +35,93 @@ class GalleryActivity : AppCompatActivity() {
                 }
             }
 
+            //테스트용 이미지를 외부저장소에 따로 넣어놔야 함
+            assetsToExternalStorage()
+
+            for(f in file.listFiles()){
+                //TODO 이미지 파일이 아닌경우 예외처리
+                //TODO 이미지를 암호화해서 저장해놓고 불러올 때만 복호화 하기
+                photoList.add(Photo(f))
+            }
+
+            if(photoList.size != 0){    //이미지가 아무것도 없는경우에는 selectedImage를 세팅하지 않음
+                selectedImage.setImageBitmap(photoList[0].image)
+                selectedImage.setTag(0)
+            }
+        } else{
+            //외부저장소가 마운트되지 않아서 파일을 읽고 쓸 수 없음
+        }
+
+        list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        list.hasFixedSize()
+        list.adapter = GalleryAdapter(this, photoList)
+    }
+
+    fun showPreviousImage(v: View){
+        try {
+            var prePosition: Int = (selectedImage.getTag() as Int) - 1
+            selectedImage.setImageBitmap(photoList[prePosition].image)
+            selectedImage.setTag(prePosition)
+        } catch(e: Exception){
+            //Toast.makeText(this, "앞이 없음", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun showNextImage(v: View){
+        try {
+            var nextPosition: Int = (selectedImage.getTag() as Int) + 1
+            selectedImage.setImageBitmap(photoList[nextPosition].image)
+            selectedImage.setTag(nextPosition)
+        } catch(e: Exception){
+            //Toast.makeText(this, "뒤가 없음", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun assetsToExternalStorage(){
+        //assets에 있는 파일을 외부저장소로 복사(테스트용)
+        for(i in 1..4){
+            var filename: String = "front_week" + i + ".jpg"
+            var assetManager: AssetManager = getAssets()
+            var input: InputStream = assetManager.open("gallery_body/" + filename)
+            var outputfile:String = getExternalFilesDir(null).toString() + "/gallery_body/" + filename
+            Log.d("file", i.toString()+"번째, " + outputfile)
+            var output: OutputStream = FileOutputStream(outputfile)
+            var buffer: ByteArray = ByteArray(1024)
+            var length: Int
+
+            do{
+                length = input.read(buffer)
+                if(length <= 0) break;
+                output.write(buffer, 0, length)
+            }while(true)
+
+            output.flush();
+            output.close();
+            input.close();
+        }
+    }
+
+/* 안쓰는 코드들
+    fun ShowSelectedImage(){
+        //외부저장소(SD카드)가 마운트되었는지 확인
+        var state: String = Environment.getExternalStorageState()
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            //디렉토리 생성
+            var filedir: String = getExternalFilesDir(null).toString() + "/gallery_body"  //Android/data/com.example.android.eyebody/files/gallery_body
+            var file: File = File(filedir)
+            if(!file.exists()){
+                if(!file.mkdirs()){
+                    //디렉토리가 만들어지지 않음
+                }
+            }
+
             var fileList = file.listFiles() //폴더 안에 있는 파일들 리스트
 
             for(f in fileList){
+                var imageView: ImageView = ImageView(this)
+                var lp: LayoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+                imageView.layoutParams = lp
+
                 //TODO RecyclerView로 갤러리 UI 만들기
                 var imageView: ImageView = ImageView(this)
                 imageView.layoutParams = ViewGroup.LayoutParams(500, ViewGroup.LayoutParams.WRAP_CONTENT)
@@ -45,11 +132,22 @@ class GalleryActivity : AppCompatActivity() {
                 var bitmapImg: Bitmap = BitmapFactory.decodeStream(fis)  //Bitmap으로 변환
                 imageView.setImageBitmap(bitmapImg)   //Bitmap까지 변환해야 이미지뷰에 띄울 수 있음
 
+                imageView.setOnClickListener {  //이미지 크게 보여주기
+                    imageViewOnClickListener(imageView)
+                    //selectedImage.setImageBitmap(bitmapImg)
+                    //selectedImage.setImageDrawable(imageView.getDrawable())
+                }
+
                 galleryLayout.addView(imageView)
             }
         } else{
             //외부저장소가 마운트되지 않아서 파일을 읽고 쓸 수 없음
         }
+    }
+
+    fun imageViewOnClickListener(v: View?){
+        var img = v as ImageView
+        selectedImage.setImageDrawable(img.getDrawable())
     }
 
     fun ExternalTextFileIO(){
@@ -150,4 +248,5 @@ class GalleryActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
+    */
 }
