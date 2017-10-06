aef55b26e0bb281891bb808faa830eb63474b5bb
mijey <yeaji21@gmail.com>
2017-10-06 11:56:02 +0900

    아이콘 이미지 변경

--- EyeBody2/app/src/main/java/com/example/android/eyebody/gallery/CollageActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/gallery/CollageActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android.eyebody.gallery
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import com.example.android.eyebody.R
 import kotlinx.android.synthetic.main.activity_collage.*
 
@@ -17,5 +20,20 @@ class CollageActivity : AppCompatActivity() {
         //RecyclerView
         collageView.hasFixedSize()
         collageView.adapter = CollageAdapter(this, photoList)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_collage, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_share -> {
+                //TODO 카카오톡 공유하기
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
