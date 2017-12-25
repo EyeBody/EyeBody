efe386ce8af173b8a69dbc87ba09dc6a31c39b8a
mijey <yeaji21@gmail.com>
2017-12-25 16:03:17 +0900

    PhotoFrameActivity에서 메모 오버레이 만듬

--- EyeBody2/app/src/main/java/com/example/android/eyebody/gallery/PhotoFrameActivity.kt
+++ EyeBody2/app/src/main/java/com/example/android/eyebody/gallery/PhotoFrameActivity.kt
@@ -2,7 +2,9 @@ package com.example.android.eyebody.gallery
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.MotionEvent
+import android.view.View
 import android.widget.Toast
 import com.example.android.eyebody.R
 import kotlinx.android.synthetic.main.activity_photo_frame.*
@@ -25,35 +27,43 @@ class PhotoFrameActivity : AppCompatActivity() {
         photoList = intent.getParcelableArrayListExtra("photoList")
         pos = intent.getIntExtra("pos", 0)
 
+        hideActionBar()
+
         //RecyclerView
         photoFrameRecyclerView.hasFixedSize()
         photoFrameRecyclerView.adapter = PhotoFrameAdapter(this, photoList)
         photoFrameRecyclerView.scrollToPosition(pos)
 
-        hideActionBar()
+        memoTextView.setMovementMethod(ScrollingMovementMethod())
+        memoTextView.text = photoList[pos].getMemo()
+        dateTextView.text = photoList[pos].getDate("yyyy년 dd월 mm일")
+
+        editMemoButton.setOnClickListener {
+            Toast.makeText(this, "메모 작성", Toast.LENGTH_SHORT).show()
+        }
 
         photoFrameRecyclerView.setOnTouchListener { view, motionEvent ->
             when (motionEvent.getAction()){
                 MotionEvent.ACTION_DOWN -> {
                     x1 = motionEvent.x
-
                 }
                 MotionEvent.ACTION_UP -> {
                     x2 = motionEvent.x
 
-                    //왼쪽에서 오른쪽으로 스와이프 -->
-                    if (x2 - x1 > minDistance && pos - 1 >= 0) {
-                        pos = pos - 1
-                    }
+                    //스와이프
+                    if(abs(x1 - x2) > minDistance){
+                        if (x1 < x2 && pos - 1 >= 0)    //왼쪽에서 오른쪽으로 스와이프 -->
+                            pos = pos - 1
+                        else if (x1 > x2 && pos + 1 < photoList.size)   //오른쪽에서 왼쪽으로 스와이프 <--
+                            pos = pos + 1
 
-                    //오른쪽에서 왼쪽으로 스와이프 <--
-                    else if (x1 - x2 > minDistance && pos + 1 < photoList.size) {
-                        pos = pos + 1
+                        dateTextView.text = photoList[pos].getDate("yyyy년 dd월 mm일")
+                        memoTextView.scrollTo(0, 0)
+                        memoTextView.text = photoList[pos].getMemo()
                     }
-
                     //짧은 터치
-                    else if(abs(x1 - x2) < 50) {
-
+                    else if (abs(x1 - x2) < 50) {
+                        toggleMemo()
                     }
 
                     photoFrameRecyclerView.smoothScrollToPosition(pos)
@@ -61,7 +71,6 @@ class PhotoFrameActivity : AppCompatActivity() {
             }
             false
         }
-
     }
 
     fun hideActionBar() {
@@ -72,4 +81,15 @@ class PhotoFrameActivity : AppCompatActivity() {
         }
     }
 
+    fun toggleMemo(){
+        if(memoTextView.visibility == View.VISIBLE){
+            dateTextView.visibility = View.INVISIBLE
+            memoTextView.visibility = View.INVISIBLE
+            editMemoButton.visibility = View.INVISIBLE
+        } else {
+            dateTextView.visibility = View.VISIBLE
+            memoTextView.visibility = View.VISIBLE
+            editMemoButton.visibility = View.VISIBLE
+        }
+    }
 }
